18f2e896b629ed9177b215a806fabf659240e527
Artik <artik.alpha@gmail.com>
2021-03-12 12:04:15 +0300

    checking errors

--- src/index.ts
+++ src/index.ts
@@ -16,102 +16,127 @@ TRANSLATE.set('today', new Set(['today', 'сегодня', 'aujourd\'hui']));
 
 
 const translate = (str: string): string => {
-    const reg = /([a-zа-яA-ZА-ЯçŞğéüûıź]+)/;
-    return str.replace(reg, word => {
-        for (const [key, set] of TRANSLATE) {
-            if (set.has(word)) {
-                return key;
-            }
-        }
-        return word;
-    });
+  const reg = /([a-zа-яA-ZА-ЯçŞğéüûıź]+)/;
+  return str.replace(reg, word => {
+    for (const [key, set] of TRANSLATE) {
+      if (set.has(word)) {
+        return key;
+      }
+    }
+    return word;
+  });
 };
 
+export const isValidDate = date => date instanceof Date && !isNaN(+date);
+
 export const parseDate = (str: string): Date => {
-    str = translate(str.toLowerCase());
-    // d.MM.YYYY
-    // d.M.YYYY
-    // dd-MM-YYYY
-    // d-M-YYYY
-    // dd.MM.YYYY hh:mm
-    // d.M.YYYY hh:mm
-    // dd-MM-YYYY, hh:mm:ss
-    const reg1 = /(\d{1,2}).(\d{1,2}).(\d{4}),?\s?(\d{1,2}:\d{1,2}(:\d{1,2})?)?/;
-    if (reg1.test(str)) {
-        str = str.replace(reg1, '$3-$2-$1 $4 ');
-        return new Date(str);
+  str = translate(str.toLowerCase());
+  // d.MM.YYYY
+  // d.M.YYYY
+  // dd-MM-YYYY
+  // d-M-YYYY
+  // dd.MM.YYYY hh:mm
+  // d.M.YYYY hh:mm
+  // dd-MM-YYYY, hh:mm:ss
+  {
+    const reg = /(\d{1,2}).(\d{1,2}).(\d{4}),?\s?(\d{1,2}:\d{1,2}(:\d{1,2})?)?/;
+    if (reg.test(str)) {
+      const date = new Date(str.replace(reg, '$3-$2-$1 $4 ').trim());
+      if (isValidDate(date)) {
+        return date;
+      }
     }
+  }
 
-    // dd MMM YYYY at h:mm
-    // dd MMM YYYY at h:mm:ss
-    // dd MMM YYYY at h:mm a
-    // dd MMM YYYY at h:mm:ss a
-    // dd MMM YYYY, hh:mm
-    // dd-MMMM-YYYY, hh:mm
-    // d MMM YYYY, hh:mm
-    // dd MMM YYYY
-    // d MMM YYYY
-    // d MMM, YYYY
-    // d MMM, YYYY - hh:mm
-    // d MMM, YYYY - hh:mm a
-    // d MMM, YYYY - hh:mm:ss
-    // d MMM, YYYY - hh:mm:ss a
-    const reg2 = /(\d{1,2})\s([a-z]{3}),?\s(\d{4})\D+(\d{1,2}:\d{1,2}(:\d{1,2})?(\s(am|pm)?))?/;
-    if (reg2.test(str)) {
-        str = str.replace(reg2, '$1 $2 $3 $4 ');
-        return new Date(str);
+  // dd MMM YYYY at h:mm
+  // dd MMM YYYY at h:mm:ss
+  // dd MMM YYYY at h:mm a
+  // dd MMM YYYY at h:mm:ss a
+  // dd MMM YYYY, hh:mm
+  // dd-MMMM-YYYY, hh:mm
+  // d MMM YYYY, hh:mm
+  // dd MMM YYYY
+  // d MMM YYYY
+  // d MMM, YYYY
+  // d MMM, YYYY - hh:mm
+  // d MMM, YYYY - hh:mm a
+  // d MMM, YYYY - hh:mm:ss
+  // d MMM, YYYY - hh:mm:ss a
+  {
+    const reg = /(\d{1,2})\s([a-z]{3}),?\s(\d{4})\D+(\d{1,2}:\d{1,2}(:\d{1,2})?(\s(am|pm)?))?/;
+    if (reg.test(str)) {
+      const date = new Date(str.replace(reg, '$1 $2 $3 $4 ').trim());
+      if (isValidDate(date)) {
+        return date;
+      }
     }
+  }
 
-    // MMM dd, YYYY, hh:mm:ss
-    // MMM dd, YYYY, hh:mm:ss a
-    // MMM dd, YYYY, hh:mm
-    // MMM dd, YYYY, hh:mm a
-    // MMM dd, YYYY
-    // MMM dd, YYYY at h:mm a
-    const reg3 = /([a-z]{3})\s(\d{1,2}),?\s(\d{4}),?(\s\D+)?\s?(\d{1,2}:\d{1,2}:?\d{1,2})?\s?(am|pm)?/;
-    if (reg3.test(str)) {
-        str = str.replace(reg3, '$1 $2 $3 $5 $6 ');
-        return new Date(str);
+  // MMM dd, YYYY, hh:mm:ss
+  // MMM dd, YYYY, hh:mm:ss a
+  // MMM dd, YYYY, hh:mm
+  // MMM dd, YYYY, hh:mm a
+  // MMM dd, YYYY
+  // MMM dd YYYY
+  // MMM dd, YYYY at h:mm a
+  {
+    const reg = /([a-z]{3})\s(\d{1,2}),?\s(\d{4}),?(\s\D+)?\s?(\d{1,2}:\d{1,2}:?\d{1,2})?\s?(am|pm)?/;
+    if (reg.test(str)) {
+      const date = new Date(str.replace(reg, '$1 $2 $3 $5 $6 ').trim());
+      if (isValidDate(date)) {
+        return date;
+      }
     }
+  }
 
-    // YYYY-MM-dd
-    // YYYY-MM-dd hh:mm
-    // YYYY-MM-dd hh:mm a
-    // YYYY-MM-dd hh:mm:ss
-    const reg4 = /(\d{4})-(\d{1,2})-(\d{1,2})\s?(\d{1,2}:\d{1,2}:?(\d{1,2})?(\s(am|pm))?)?/;
-    if (reg4.test(str)) {
-        str = str.replace(reg4, '$1-$2-$3 $4 ');
-        return new Date(str);
+  // YYYY-MM-dd
+  // YYYY.MM.dd
+  // YYYY/MM/dd
+  // YYYY-MM-dd hh:mm
+  // YYYY-MM-dd hh:mm a
+  // YYYY-MM-dd hh:mm:ss
+  {
+    const reg = /(\d{4})[-./](\d{1,2})[-./](\d{1,2})\s?(\d{1,2}:\d{1,2}:?(\d{1,2})?(\s(am|pm))?)?/;
+    if (reg.test(str)) {
+      const date = new Date(str.replace(reg, '$1-$2-$3 $4 ').trim());
+      if (isValidDate(date)) {
+        return date;
+      }
     }
+  }
 
-    // Сегодня, hh:mm
-    // today at h:mm a
-    // today at h:mm:ss a
-    // Вчера, hh:mm
-    // yesterday at h:mm a
-    // yesterday at h:mm:ss a
-    const reg5 = /(yester|to)day(,)?\s(at\s)?((\d{1,2}):(\d{1,2}):?(\d{1,2})?(\s(am|pm))?)/;
-    if (reg5.test(str)) {
-        const parsed = new Date();
-        const [h, m, s, a] = str.replace(reg5, '$5/$6/$7/$8').split('/');
-        if (h.length) {
-            let hours = +h;
-            if (a.indexOf('pm') !== -1) {
-                hours += 12;
-            }
-            parsed.setHours(hours);
+  // Сегодня, hh:mm
+  // today at h:mm a
+  // today at h:mm:ss a
+  // Вчера, hh:mm
+  // yesterday at h:mm a
+  // yesterday at h:mm:ss a
+  {
+    const reg = /(yester|to)day(,)?\s(at\s)?((\d{1,2}):(\d{1,2}):?(\d{1,2})?(\s(am|pm))?)/;
+    if (reg.test(str)) {
+      const date = new Date();
+      const [h, m, s, a] = str.replace(reg, '$5/$6/$7/$8').split('/');
+      if (h.length) {
+        let hours = +h;
+        if (a.indexOf('pm') !== -1) {
+          hours += 12;
         }
-        if (m.length) {
-            parsed.setMinutes(+m);
-        }
-        if (s.length) {
-            parsed.setSeconds(+s);
-        }
-        if (/yesterday/.test(str)) {
-            parsed.setDate(parsed.getDate() - 1);
-        }
-        return parsed;
+        date.setHours(hours);
+      }
+      if (m.length) {
+        date.setMinutes(+m);
+      }
+      if (s.length) {
+        date.setSeconds(+s);
+      }
+      if (/yesterday/.test(str)) {
+        date.setDate(date.getDate() - 1);
+      }
+      if (isValidDate(date)) {
+        return date;
+      }
     }
+  }
 
-    throw new Error(`[Date parsing error] : ${str}`);
+  throw new Error(`[Date parsing error] : ${ str }`);
 };
